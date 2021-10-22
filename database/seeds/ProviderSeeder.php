714248ac4ab9302c9d81bb2ef04ae147c7fd1eb9
Princy S <princy.s@OPN-MC-210.local>
2021-10-22 17:23:36 +0530

    providers table and seeder

---
+++ database/seeds/ProviderSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ProviderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         
+         DB::table('providers')->insert([
+            'address'=>'bankopen',
+            'name'=>'princy',
+            'email'=>'princy@gmail.com',
+            'password'=>Hash::make('12345'),
+            'phone_number'=>'123456789',
+            'category'=>'provider'
+        ]);
+
+        DB::table('providers')->insert([
+            'address'=>'bankopen',
+            'name'=>'anandhu',
+            'email'=>'anandhu@gmail.com',
+            'password'=>Hash::make('12345'),
+            'phone_number'=>'0123456789',
+            'category'=>'provider'
+        ]);
+
+        DB::table('providers')->insert([
+            'address'=>'bankopen',
+            'name'=>'robin',
+            'email'=>'robin@gmail.com',
+            'password'=>Hash::make('12345'),
+            'phone_number'=>'2345678901', 
+            'category'=>'provider'
+        ]);
+    }
+
+}
