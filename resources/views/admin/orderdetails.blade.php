2c0e618df4b0e7a9e854916413ffe7b848cead90
Robin Babu <robin.babu@OPN-MC-012.local>
2021-10-25 13:22:51 +0530

    orderdetails_initial

---
+++ resources/views/admin/orderdetails.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Details</title>
+    <link rel="stylesheet" href="orderdetails.css">
+</head>
+<body>
+    <div class="order">
+    <span>Order Details</span>
+    <table>
+        
+        <tr><td>Order No</td><td>123</td></tr>
+        <tr><td>Date</td><td>25/10/2021</td></tr>
+        
+
+    </table>
+    <span>Buyer Information</span>
+    <table>
+        
+        <tr><td>Name</td><td>Robin</td></tr>
+        <tr><td>Email</td><td>robinbabu@gmail.com</td></tr>
+        <tr><td>Phone</td><td>123245</td></tr>
+        
+
+    </table>
+    <span>Product Details</span>
+    <table>
+    <table>
+        <tr><th>No.</th><th>Product Name</th><th>Unit Price</th><th>Quantity</th><th>Subtotal</th></tr>
+        <tr><td>1</td><td>Shoe</td><td>600</td><td>2</td><td>1200</td></tr>
+        <tr><td>2</td><td>Shirt</td><td>800</td><td>1</td><td>800</td></tr>
+        <tr><td>3</td><td>Food kit</td><td>300</td><td>3</td><td>1200</td></tr>
+    </table>
+
+<div class="orderbottom">
+    <input type="button" id="backtohistory" value="Back to History">
+    <input type="button" id="payment" value="payment">
+
+    </div>
+
+
+    </div>
+</body>
+</html>
