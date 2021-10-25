929e00a53986d212c43e449fad8a92c440672f25
Sam Sunil <sam.sunil@OPN-MC-092.local>
2021-10-25 11:14:41 +0530

    welcom page mod

--- database/migrations/2021_10_22_080637_ordertable.php
+++ database/migrations/2021_10_22_080637_ordertable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Ordertable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('order', function(Blueprint $table)
+        { 
+            $table->bigIncrements('id');
             $table->timestamps();
+            $table->string('products');
+            $table->string('price');
+            $table->string('status');
+            $table->string('phone');
+            $table->string('Date & Time');
+            
+        
+        
         });
     }
 
@@ -30,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
